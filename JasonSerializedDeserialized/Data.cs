f3cc4f15b41f4d1a6ca3cccef8eb71367f43a6d4
Siamak.G <89330291+SiamakGoudarzi@users.noreply.github.com>
2026-02-25 15:17:16 +0100

    Add files via upload

---
+++ JasonSerializedDeserialized/Data.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JasonSerializedDeserialized
+{
+   public class Data
+    {
+
+        [JsonProperty("price")]
+        public string[] Price { get; set; }
+        [JsonProperty("offerprice")]
+        public string[] OfferPrice { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("title_en")]
+        public string TitleEn { get; set; }
+        [JsonProperty("color")]
+        public string[] Color { get; set; }
+        [JsonProperty("color_hex")]
+        public string[] ColorHex { get; set; }
+        [JsonProperty("img")]
+        public string Img { get; set; }
+        [JsonProperty("status")]
+        public string Status { get; set; }
+        [JsonProperty("availability")]
+        public string Availability { get; set; }
+
+        [JsonIgnore]
+        public Dictionary<string, Tuple<int, int>> PriceAndOfferpriceByColor { get; set; } = new Dictionary<string, Tuple<int, int>>();
+
+
+
+    }
+}
